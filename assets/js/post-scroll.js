b779de00c75e151dc64ae4903f7c9e0d4ada6714
peetonn <gpeetonn@gmail.com>
2019-02-10 23:50:12 -0800

    initial implementtation of the project page scrolling and svg animation

---
+++ assets/js/post-scroll.js
@@ -0,0 +1,182 @@
+
+var scrollController = null;
+var group = null;
+
+function setupPathAnimation(svgGroup, duration)
+{
+  var allPaths = $(svgGroup).find('path');
+  // console.log('will animate '+allPaths.length+' paths');
+
+  var pathsTween = [];
+  allPaths.each(function(idx, p){
+    var pLen = p.getTotalLength();
+    var tween = TweenMax.fromTo(p,duration,
+      {
+        opacity: '1',
+        strokeDasharray:String(pLen)+" "+String(pLen),
+        strokeDashoffset: String(pLen)
+      },
+      {
+        strokeDashoffset: '0'
+      });
+    // check for marker-ends
+    if ($(p).attr('marker-end'))
+    {
+      var markerEnd = $(p).attr('marker-end');
+      // nullify now
+      $(p).attr('marker-end','');
+      // trigger at the end of path drawing
+      tween.eventCallback('onComplete', function(){
+        $(p).attr('marker-end',markerEnd);
+      });
+      tween.eventCallback('onReverseComplete', function(){
+        $(p).attr('marker-end','');
+      });
+    }
+    pathsTween.push(tween);
+  });
+
+  return pathsTween;
+}
+
+function setupBlockAnimation(svgGroup, duration)
+{
+  return TweenMax.fromTo(svgGroup, duration,
+    {
+      opacity: '0'
+    },
+    {
+      opacity: '1'
+    });
+}
+
+function setupAnimationStep(annotationScene, svgAnimationGroup)
+{
+  var steps = $(svgAnimationGroup).find('g');
+  console.log('found animation steps '+steps.length);
+
+  var stepDuration = 1./steps.length;
+  var animationTimeline = new TimelineMax();
+  steps.each(function(idx, s){
+    var cl = $(s).attr('class');
+    var stepTween = null;
+
+    if (cl == 'svg-anim-path')
+      stepTween = setupPathAnimation(s, stepDuration);
+    else
+      stepTween = setupBlockAnimation(s, stepDuration);
+
+    animationTimeline.add(stepTween, idx*stepDuration, 'start');
+  });
+
+  svgAnimationGroup.style.opacity = "1";
+  annotationScene.setTween(animationTimeline);
+}
+
+function setupScrollScenes() {
+
+  if (scrollController)
+    delete scrollController;
+  scrollController = new ScrollMagic.Controller({logLevel:0});
+
+  // set up project header pin
+  var header = $('.project-page header')[0];
+  var headerScene = new ScrollMagic.Scene({
+    triggerElement: header,
+    triggerHook: 0
+  }).setPin(header);
+  scrollController.addScene(headerScene);
+
+  // NOTE:
+  // the page structure is assumed to follow this rule:
+  // - all direct children of .project-content are either annotations or content
+  // - annotations and content are separated in two columns (using css grid)
+  // - it is assumed that even children are annotations (left column)
+  //    and odd children are content
+  var allAnnotations = $('.project-content').children().filter(':even');
+  var allContents = $('.project-content').children().filter(':odd');
+
+  // setup pinning for every paragraph
+  var annScrollScenes = {};
+  var h = Math.max(document.documentElement.clientHeight, window.innerHeight || 0);
+  var scrollHook = header.clientHeight/h;
+
+  // console.log('viewport h '+h+' header '+header.offsetHeight);
+  allAnnotations.each(function(idx, p){
+    if (idx < allContents.length)
+    {
+      var imgBlock = allContents[idx];
+      var scrollDuration = imgBlock.clientHeight-p.clientHeight;
+
+
+      // console.log(idx+' img block ' + imgBlock.clientHeight + ' para '+p.clientHeight+' dur '+scrollDuration);
+      var scrollScene = new ScrollMagic.Scene({
+          triggerElement:p,
+          triggerHook:scrollHook,
+          duration:scrollDuration
+        }).setPin(p);
+      annScrollScenes[idx] = scrollScene;
+      scrollController.addScene(scrollScene);
+    }
+  });
+
+  // setup SVG animation, if SVG exists on the page
+  // for SVG to animate:
+  // - have inline SVG
+  // - inside <svg> have animation steps split into groups using <g>
+  // - annotation paragraphs number should be equal to animation group number
+  // - for every SVG animation step annotation paragraph, have a phony content
+  //    paragraph (see page layout rules above) with class "svg-scroll-step"
+  // - animation group 0 is visible by default -- this is the start-off, all
+  //    other groups are hidden by default
+  if ($('.scrollable-svg').length)
+  {
+    var svg = $('.scrollable-svg')[0];
+    var animationGroups = $('.scrollable-svg>svg>g');
+    var svgIdx = null;
+
+    allContents.each(function(idx, p){
+      if (p == svg) svgIdx = idx;
+    });
+
+    // get paragraphs for svg
+    var svgStepAnnotations = allAnnotations.slice(svgIdx,animationGroups.length);
+    var scrollParagraphs = $('.svg-scroll-step');
+
+    // adjust heights of SVG animation phony content elements
+    scrollParagraphs.each(function(idx, p){
+      p.style.height = String(svg.offsetHeight)+'px';
+    });
+
+    var svgPinDuration = (animationGroups.length-1)*svg.offsetHeight;
+
+    var svgPin = new ScrollMagic.Scene({
+      triggerElement: svg,
+      triggerHook: scrollHook,
+      duration: svgPinDuration
+    }).setPin(svg, {pushFollowers: false});
+    // svgPin.addIndicators({
+    //   name: 'triggerDown', // custom name for your scene
+    //   indent: 520, // indent from the browser edge
+    //   colorStart: 'yellow', // custom color - colorEnd
+    //   colorTrigger: 'yellow',
+    // });
+    scrollController.addScene(svgPin);
+
+    // setup scenes for every animation group
+    // by default, hide all groups except first one
+    animationGroups.each(function(idx, g){
+      if (idx > 0)
+      {
+        // g.style.opacity = "0";
+        setupAnimationStep(annScrollScenes[idx-1], g);
+      }
+    });
+
+    // now we're ready for animation
+    svg.style.opacity = "1";
+  }
+}
+
+$( document ).ready(setupScrollScenes);
+// $( window ).resize(setupScrollScenes);
